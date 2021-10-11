59da1eeff209593d2bed3edebf5838400371156a
Ariane Rocha <arianerochaf@gmail.com>
2021-10-11 15:45:23 -0300

    alteracoes

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -1,6 +1,6 @@
 package lambdas;
 
-@FunctionalInterface // >> marca que a interface terá apenas um unico metodo - VALIDAÇÃO
+@FunctionalInterface // >> marca que a interface terá apenas um unico metodo abstrato- VALIDAÇÃO
 // ESTRATÉGIA, que garante o uso funcional das lambdas
 public interface Calculo {
     double executar (double a, double b);
